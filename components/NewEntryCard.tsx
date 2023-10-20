39e155844c1aff5bf1e8e10ec6f8003cfc136d5d
sarthakastic <iamsg26@gmail.com>
2023-10-20 23:54:46 +0530

    add : new journal

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { createNewEntry } from "@/utils/api";
+import { useRouter } from "next/navigation";
+
 const NewEntryCard = () => {
-  const handleOnClick = () => {};
+  const router = useRouter();
+
+  const handleOnClick = async () => {
+    const data = await createNewEntry();
+    router.push(`/journal/${data.id}`);
+  };
 
   return (
     <div
--- utils/api.ts
+++ utils/api.ts
@@ -1,3 +1,18 @@
+import { POST } from "@/app/api/journal/route";
+
 const createURL = (path) => {
   return window.location.origin + path;
+};
+
+export const createNewEntry = async () => {
+  const res = await fetch(
+    new Request(createURL("/api/journal"), {
+      method: "POST",
+    })
+  );
+
+  if (res.ok) {
+    const data = await res.json();
+    return data.data;
+  }
 };
